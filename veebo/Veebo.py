8f1595be1188df749ce290eacff0574b61a39cd7
D34dMan <shibinkidd@gmail.com>
2016-10-24 10:05:10 +0200

    Logging and Plugin activation controlled from profile.yaml

--- veebo/Veebo.py
+++ veebo/Veebo.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import logging.config
 import threading
 from state_machine import *
 
-from yapsy.PluginManager import PluginManager
-from EventDispatcher import EventDispatcher
+# from yapsy.PluginManager import PluginManager
+from veebo.VeeboPluginManager import VeeboPluginManager
+from veebo.interfaces.SystemPluginBase import SystemPluginBase
+from veebo.interfaces.DefaultPluginBase import DefaultPluginBase
+from veebo.EventDispatcher import EventDispatcher
 import os
 import yaml
 
@@ -42,9 +46,8 @@ class Veebo():
     quit = Event(from_states=(initializing, sleeping, listening, processing),
                  to_state=stopping)
 
-
-
     def __init__(self):
+        self.logger = logging.getLogger('veebo')
         self.event_dispatcher = EventDispatcher()
         self.path = {}
         self.path['app'] = os.path.normpath(
@@ -78,10 +81,13 @@ class Veebo():
         try:
             with open(self.path['config_file'], "r") as f:
                 self.config = yaml.safe_load(f)
+                logging.config.dictConfig(self.config['logging'])
 
         except OSError:
-            logging.error("Can't open config file: '%s'", self.path['config_file'])
+            self.logger.error("Can't open config file: '%s'",
+                          self.path['config_file'])
             raise
+
 
     def health_check_config_dir(self):
         # Check if config direcotry present
@@ -90,14 +96,14 @@ class Veebo():
                 # Try creating the config directory
                 os.makedirs(self.path['config'])
             except OSError:
-                logging.error("Can't create configuration directory: '%s'",
-                      self.path['config'])
+                self.logger.error("Can't create configuration directory: '%s'",
+                              self.path['config'])
                 raise
 
         # Check if config directory is writable
         if not os.access(self.path['config'], os.W_OK):
-            logging.error("Configuration directory '%s' is not writable.",
-                  self.path['config'])
+            self.logger.error("Configuration directory '%s' is not writable.",
+                          self.path['config'])
 
     def __del__(self):
         self.plugin_manager = None
@@ -106,18 +112,37 @@ class Veebo():
             self.quit()
 
     def initializePlugins(self):
-        self.plugin_manager = PluginManager()
+        plugin_categories = {
+            "System": SystemPluginBase,
+            "Default": DefaultPluginBase,
+        }
+        self.plugin_manager = VeeboPluginManager()
         self.plugin_manager.setPluginPlaces(
             [self.path['plugins'], self.path['user_plugins']])
+
+        self.plugin_manager.setCategoriesFilter(plugin_categories)
         # Load the plugins from the plugin directory.
         self.plugin_manager.collectPlugins()
         thread_list = []
-        for plugin in self.plugin_manager.getAllPlugins():
-            plugin.plugin_object.init(self)
-            if "run" in dir(plugin.plugin_object):
-                t = threading.Thread(target=plugin.plugin_object.run)
-                thread_list.append(t)
 
+        for plugin_category in self.config['plugins']['active']:
+            self.plugin_manager.getPluginsOfCategory(plugin_category)
+            for plugin_name in self.config['plugins']['active'][plugin_category]:
+                plugin = self.plugin_manager.getPluginByName(
+                    plugin_name,
+                    category=plugin_category)
+                if plugin:
+                    self.logger.debug("Activating plugin %s in %s", plugin_name, plugin_category)
+                    self.plugin_manager.activatePluginByName(
+                        plugin.name,
+                        category=plugin_category,
+                        veebo=self)
+                    if "run" in dir(plugin.plugin_object):
+                        self.logger.debug("Run method found in plugin %s in %s", plugin_name, plugin_category)
+                        t = threading.Thread(target=plugin.plugin_object.run)
+                        thread_list.append(t)
+                else:
+                    self.logger.error("Plugin not found %s in %s", plugin_name, plugin_category)
 
         for thread in thread_list:
             thread.start()
